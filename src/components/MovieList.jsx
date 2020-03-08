a2a050da790f3235fc7a95d890f0dc8036fdd0e9
Ivo Pontes <ivopontes@outlook.com>
2020-03-08 15:14:48 -0300

    Funcionalidade de Lista de Filmes [OK]

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,29 +1,27 @@
 import React from 'react';
 import {Card, ListGroup, ListGroupItem} from 'react-bootstrap';
+import '../styles/MovieList.css';
 
-
-function MovieList({title,synopsis, year, image}) {
+function MovieList({title,synopsis, year, image, trailer, opacity}) {
   return (
-          <>
-              <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={image} />
-                <Card.Body>
-                  <Card.Title style={{ minHeight: '3em'}}>{title}</Card.Title>
-                  <Card.Text>
-                    {synopsis}
-                  </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                  <ListGroupItem></ListGroupItem>
-                  <ListGroupItem><b>Year: </b> {year}</ListGroupItem>
-                  <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                  <ListGroupItem>Vestibulum at eros</ListGroupItem>
-                </ListGroup>
-                <Card.Body>
-                  <Card.Link href="#">Card Link</Card.Link>
-                </Card.Body>
-              </Card>
-          </>);
+    <>
+        <Card style={{ width: '20rem', height: '38.125rem', margin: '1em 0em', opacity: `${opacity}` }}>
+          <Card.Img variant="top" src={image} />
+          <Card.Body>
+            <Card.Title style={{ minHeight: '2em'}}>{title}</Card.Title>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem></ListGroupItem>
+            <ListGroupItem><b>Year: </b> {year}</ListGroupItem>
+            <ListGroupItem className="text-justify" style={{height: '13.563rem'}}>
+              {synopsis}
+            </ListGroupItem>
+          </ListGroup>
+          <Card.Body>
+            <Card.Link href={trailer} target="_blank">Trailer</Card.Link>
+          </Card.Body>
+        </Card>
+    </>);
 }
 
 export default MovieList;
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -2,12 +2,14 @@ import React, {useEffect, useState} from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
 import '../styles/SearchForm.css';
 import MovieList from './MovieList';
+import ImageNotFound from '../assets/imgs/not-found.png';
 
 function SearchForm() {
   const [search, setSearch] = useState('');
   const [query, setQuery] = useState(false);
   const [movies, setMovies] = useState([]);
   const endpoint = 'https://tv-v2.api-fetch.website/movies/1?sort=rating&order=-1&genre=all&keywords=';
+
 
   useEffect(() => {
     console.log("Effect has ben run");
@@ -46,12 +48,27 @@ function SearchForm() {
       </form>
       <Container>
         <Row className="justify-content-md-center movie-list">
-          {movies.map((movie, index) => (
-            <Col md="auto" key={movie.imdb_id + index}>
-              <MovieList title={movie.title} synposis={movie.synopsis}
-              year={movie.year} image={movie.images['fanart']}/>
-            </Col>
-          ))}
+          {movies.map((movie, index) => {
+            
+            var card_opacity = 1;
+            if(movie.genres.includes('horror'))
+              card_opacity = 1;
+            else
+              card_opacity = 0.5;
+              
+            var img = '';
+            if(movie.images['fanart'])
+              img = movie.images['fanart'];
+            else
+              img = ImageNotFound;
+              
+
+           return (<Col md="auto" key={movie.imdb_id + index}>
+              <MovieList title={movie.title} synopsis={movie.synopsis}
+              year={movie.year} image={img} trailer={movie.trailer}
+              opacity={card_opacity}/>
+            </Col>);
+          })}
         </Row>
       </Container>
     </>
