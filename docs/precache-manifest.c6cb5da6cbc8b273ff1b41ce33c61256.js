50bf961840371bc637f1bdff21f83c4ec4bba884
Ivo Pontes <ivopontes@outlook.com>
2020-03-08 15:24:30 -0300

    Teste Github Pages

---
+++ docs/precache-manifest.c6cb5da6cbc8b273ff1b41ce33c61256.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd64f3c153c7338692d28887e198ee16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f94176c530bfd89bf463",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ee4eb2b6f683c3c4bcd5",
+    "url": "/static/css/main.70799ee3.chunk.css"
+  },
+  {
+    "revision": "f94176c530bfd89bf463",
+    "url": "/static/js/2.a8300819.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/static/js/2.a8300819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee4eb2b6f683c3c4bcd5",
+    "url": "/static/js/main.8edf670b.chunk.js"
+  },
+  {
+    "revision": "fa83d89ddc4b00aadf2a",
+    "url": "/static/js/runtime-main.8a3365f3.js"
+  },
+  {
+    "revision": "96c7c39a4d272633f7d5f87af336933a",
+    "url": "/static/media/not-found.96c7c39a.png"
+  },
+  {
+    "revision": "86be234c0e3c6616ff0364d15ef71e7f",
+    "url": "/static/media/scream_real.86be234c.ttf"
+  }
+]);
